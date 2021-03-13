6784065a87ca1de29bd96695395b961c7e63ab7e
Rifat <rifat@softbyteit.com>
2021-03-14 00:47:02 +0600

    --Add image storage location.

--- upload-image-ui/src/app/public/upload-image/upload-image.component.ts
+++ upload-image-ui/src/app/public/upload-image/upload-image.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ImageService} from '../../service/image.service';
+import {ApiUrlService} from '../../service/api-url.service';
 
 @Component({
   selector: 'app-upload-image',
@@ -10,7 +11,7 @@ export class UploadImageComponent implements OnInit {
   imageUrl: any;
   fileToUpload: File = null;
 
-  constructor(private imageService: ImageService) { }
+  constructor(private imageService: ImageService, private global: ApiUrlService) { }
 
   ngOnInit(): void {
     this.imageUrl = '/assets/image/default-image.png';
@@ -29,10 +30,11 @@ export class UploadImageComponent implements OnInit {
   OnSubmit(Caption, Image) {
     this.imageService.postFile(Caption.value, this.fileToUpload).subscribe(
       data => {
+        const url = this.global.imageUrl;
         console.log(data);
         Caption.value = null;
         Image.value = null;
-        this.imageUrl = data.imageUrl;
+        this.imageUrl = url + data.name;
       }
     );
   }
--- upload-image-ui/src/app/service/api-url.service.ts
+++ upload-image-ui/src/app/service/api-url.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 export class ApiUrlService {
 
   public apiUrl = 'http://localhost:9001/';
-  public uploadImage = this.apiUrl + 'image/upload';
-  public imageUrl = this.apiUrl + 'images';
+  public uploadImage = this.apiUrl + 'image/upload/';
+  public imageUrl = this.apiUrl + 'images/';
 
   constructor() { }
 }
